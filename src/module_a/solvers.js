ce965702f38f6405a96de0201b6c8c993e2b832a
Julian <jdssan5@gmail.com>
2026-06-20 11:34:14 -0400

    Testing

--- src/module_a/solvers.js
+++ src/module_a/solvers.js
@@ -1,1 +1,18 @@
-//Test
+// array = holding values
+// 
+
+let fruits = ["apple", "orange", "banana"];
+
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[2]);
+
+fruits.push("coco");
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+console.log(fruits);
+
